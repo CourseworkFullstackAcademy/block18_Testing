5ab1134e13a0cc431675faa39627efa9f7e87e5f
DebFullstack <debbieking1979@gmail.com>
2023-05-06 12:26:28 -0400

    finished unit tests, started functional tests

--- test.js
+++ test.js
@@ -1,1 +1,35 @@
+/*Unit Test 1: 
+A function called "multiplication" that returns the product of the two input numbers.
 
+Expect multiplication(x, y) to be a number
+Expect multiplication(x, y) to be equal to xy
+Expect multiplication("x", y) to be an error
+Expect multiplication( , ) to be an error
+Expect multiplication(x, "y" ) to be an error
+Expect multiplication(x, √(-1) ) to be an error
+
+*/
+
+/*Unit Test 2:
+A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+Example: concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1])
+...should result in [-1, 1, 3, 9, 15]
+
+Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be [-1, 1, 3, 9, 15]
+Expect concatOdds("string", [9, 1, 1, 1, 4, 15, -1]) to be an error
+Expect concatOdds([3, 2, 1], "string") to be an error
+Expect concatOdds( , [9, 1, 1, 1, 4, 15, -1]) to be an error
+Expect concatOdds(["a", "b", "c"], ["d", "e", "f", "g", "h"]) to be an error
+Expect concatOdds([3, "2", 1], ["9", 1, 1, 1, 4, 15, -1]) to be [-1, 1, 3, 15]
+Expect ([3.7, 2, 1], [9, 1, 1, 1, 4, 15.89, -1]) to be [-1, 1, 9]
+
+*/
+
+
+/*Functional Tests for:
+A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+
+1. When a user clicks on checkout in shopping cart, they should be shown a prompt with options to check out as a guest, to create an account or to log in if they are not already logged in.
+
+
+*/
