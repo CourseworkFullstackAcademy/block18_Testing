c1747febb53084231d55cc8e891498d4b2840632
DebFullstack <debbieking1979@gmail.com>
2023-05-06 12:34:58 -0400

    finsished

--- test.js
+++ test.js
@@ -29,7 +29,12 @@ Expect ([3.7, 2, 1], [9, 1, 1, 1, 4, 15.89, -1]) to be [-1, 1, 9]
 /*Functional Tests for:
 A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
 
-1. When a user clicks on checkout in shopping cart, they should be shown a prompt with options to check out as a guest, to create an account or to log in if they are not already logged in.
+1. When a user clicks on checkout in shopping cart, they should be shown a prompt with options to check out as a guest, to create an account or to log in if they are not already logged in. There should be a message that says "Log in or Create an Account for Faster Checkout for Future Orders."
 
+2. When a user clicks checkout with an empty shopping cart, they should be shown a message that says "Please at least one item to your cart to check out. Here hare some great deals we have to choose from! (Show some deals of popular products)"
+
+3. When a user clicks log in after clicking checkout, give the option to log in with email and password or to log in with Google.
+
+4. When a user logs in  and has credit card and shipping address info saved, give option to use this instead of typing it in again.
 
 */
